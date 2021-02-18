c53f3c29785f565ae8339c3200e07a4504e6632e
Rahul Sharma <rahulsharma@192.168.1.7>
2021-02-18 23:45:08 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FiltersComponent } from './filters/filters.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltersComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
