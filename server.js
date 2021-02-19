db96f7a4767811137e505141bfec94f7f9d1d8a0
Rahul Sharma <rahulsharma@192.168.1.7>
2021-02-19 16:07:02 +0530

    fixed package

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/spacex'));
 
-app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/spacex/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile('./dist/spacex/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 // Start the app by listening on the default Heroku port
